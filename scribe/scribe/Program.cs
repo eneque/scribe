4823432003c87a473882c23c404958d5f663adc9
Borodin Eugene <evgenkuropatkin@gmail.com>
2020-01-19 21:15:39 +0300

    Big BANG!!!!

---
+++ scribe/scribe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scribe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
